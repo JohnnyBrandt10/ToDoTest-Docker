771785e00be54ac55aea0e7503185bc6511355f4
Johnny Brandt <johnnybrandt10@gmail.com>
2026-07-11 12:57:28 +0300

    Rectif

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}
+        <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
+<script dangerouslySetInnerHTML={{ __html: `eruda.init();` }} />
+      </body>
     </html>
   );
 }
